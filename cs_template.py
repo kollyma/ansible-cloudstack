ff9d6adbf2af6f573c04d29e3e572290b27bb6ab
Martin Kolly <martin.kolly@swisstxt.ch>
2015-06-01 07:20:52 +0200

    add more choices for hypervisor in template creation

--- cs_template.py
+++ cs_template.py
@@ -91,7 +91,8 @@ options:
   hypervisor:
     description:
       - the target hypervisor for the template. If no hypervisor is provided, first found is used.
-    required: false
+    required: True
+    choices: [ 'KVM', 'VMware', 'BareMetal', 'XenServer', 'LXC', 'HyperV', 'UCS', 'OVM' ]
   requires_hvm:
     description:
       - true if this template requires HVM.
@@ -120,6 +121,7 @@ options:
   format:
     description: 
       - the format for the template. Required if C(vm=null).
+    # TODO add choices for format
     required: false
     default: null
   is_extractable:
@@ -148,6 +150,11 @@ options:
     required: false
     default: 'present'
     choices: [ 'present', 'absent' ]
+  poll_async:
+    description:
+      - Poll async jobs until job has finished.
+    required: false
+    default: true
 '''
 
 EXAMPLES = '''
@@ -199,253 +206,30 @@ try:
 except ImportError:
     has_lib_cs = False
 
-
-class AnsibleCloudStack:
-
-    def __init__(self, module):
-        if not has_lib_cs:
-            module.fail_json(msg="python library cs required: pip install cs")
-
-        self.module = module
-        self._connect()
-
-        self.project = None
-        self.ip_address = None
-        self.zone = None
-        self.vm = None
-        self.os_type = None
-        self.hypervisor = None
-        self.capabilities = None
-
-
-    def _connect(self):
-        api_key = self.module.params.get('api_key')
-        api_secret = self.module.params.get('secret_key')
-        api_url = self.module.params.get('api_url')
-        api_http_method = self.module.params.get('api_http_method')
-
-        if api_key and api_secret and api_url:
-            self.cs = CloudStack(
-                endpoint=api_url,
-                key=api_key,
-                secret=api_secret,
-                method=api_http_method
-                )
-        else:
-            self.cs = CloudStack(**read_config())
-
-
-    def _has_changed(self, want_dict, current_dict, only_keys=None):
-        for key, value in want_dict.iteritems():
-
-            # Optionally limit by a list of keys
-            if only_keys and key not in only_keys:
-                continue;
-
-            if key in current_dict:
-
-                # API returns string for int in some cases, just to make sure
-                if isinstance(value, int):
-                    current_dict[key] = int(current_dict[key])
-                elif isinstance(value, str):
-                    current_dict[key] = str(current_dict[key])
-
-                # Only need to detect a singe change, not every item
-                if value != current_dict[key]:
-                    return True
-        return False
-
-
-    def _get_by_key(self, key=None, my_dict={}):
-        if key:
-            if key in my_dict:
-                return my_dict[key]
-            self.module.fail_json(msg="Something went wrong: %s not found" % key)
-        return my_dict
-
-
-    # TODO: for backward compatibility only, remove if not used anymore
-    def get_project_id(self):
-        return self.get_project(key='id')
-
-
-    def get_project(self, key=None):
-        if self.project:
-            return self._get_by_key(key, self.project)
-
-        project = self.module.params.get('project')
-        if not project:
-            return None
-
-        projects = self.cs.listProjects(listall=True)
-        if projects:
-            for p in projects['project']:
-                if project in [ p['name'], p['displaytext'], p['id'] ]:
-                    self.project = p
-                    return self._get_by_key(key, self.project)
-        self.module.fail_json(msg="project '%s' not found" % project)
-
-
-    # TODO: for backward compatibility only, remove if not used anymore
-    def get_ip_address_id(self):
-        return self.get_ip_address(key='id')
-
-
-    def get_ip_address(self, key=None):
-        if self.ip_address:
-            return self._get_by_key(key, self.ip_address)
-
-        ip_address = self.module.params.get('ip_address')
-        if not ip_address:
-            self.module.fail_json(msg="IP address param 'ip_address' is required")
-
-        args = {}
-        args['ipaddress'] = ip_address
-        args['projectid'] = self.get_project(key='id')
-        ip_addresses = self.cs.listPublicIpAddresses(**args)
-
-        if not ip_addresses:
-            self.module.fail_json(msg="IP address '%s' not found" % args['ipaddress'])
-
-        self.ip_address = ip_addresses['publicipaddress'][0]
-        return self._get_by_key(key, self.ip_address)
-
-
-    # TODO: for backward compatibility only, remove if not used anymore
-    def get_vm_id(self):
-        return self.get_vm(key='id')
-
-
-    def get_vm(self, key=None):
-        if self.vm:
-            return self._get_by_key(key, self.vm)
-
-        vm = self.module.params.get('vm')
-        if not vm:
-            self.module.fail_json(msg="Virtual machine param 'vm' is required")
-
-        args = {}
-        args['projectid'] = self.get_project(key='id')
-        args['zoneid'] = self.get_zone(key='id')
-        vms = self.cs.listVirtualMachines(**args)
-        if vms:
-            for v in vms['virtualmachine']:
-                if vm in [ v['name'], v['displayname'], v['id'] ]:
-                    self.vm = v
-                    return self._get_by_key(key, self.vm)
-        self.module.fail_json(msg="Virtual machine '%s' not found" % vm)
-
-
-    # TODO: for backward compatibility only, remove if not used anymore
-    def get_zone_id(self):
-        return self.get_zone(key='id')
-
-
-    def get_zone(self, key=None):
-        if self.zone:
-            return self._get_by_key(key, self.zone)
-
-        zone = self.module.params.get('zone')
-        zones = self.cs.listZones()
-
-        # use the first zone if no zone param given
-        if not zone:
-            self.zone = zones['zone'][0]
-            return self._get_by_key(key, self.zone)
-
-        if zones:
-            for z in zones['zone']:
-                if zone in [ z['name'], z['id'] ]:
-                    self.zone = z
-                    return self._get_by_key(key, self.zone)
-        self.module.fail_json(msg="zone '%s' not found" % zone)
-
-
-    # TODO: for backward compatibility only, remove if not used anymore
-    def get_os_type_id(self):
-        return self.get_os_type(key='id')
-
-
-    def get_os_type(self, key=None):
-        if self.os_type:
-            return self._get_by_key(key, self.zone)
-
-        os_type = self.module.params.get('os_type')
-        if not os_type:
-            return None
-
-        os_types = self.cs.listOsTypes()
-        if os_types:
-            for o in os_types['ostype']:
-                if os_type in [ o['description'], o['id'] ]:
-                    self.os_type = o
-                    return self._get_by_key(key, self.os_type)
-        self.module.fail_json(msg="OS type '%s' not found" % os_type)
-
-
-    def get_hypervisor(self):
-        if self.hypervisor:
-            return self.hypervisor
-
-        hypervisor = self.module.params.get('hypervisor')
-        hypervisors = self.cs.listHypervisors()
-
-        # use the first hypervisor if no hypervisor param given
-        if not hypervisor:
-            self.hypervisor = hypervisors['hypervisor'][0]['name']
-            return self.hypervisor
-
-        for h in hypervisors['hypervisor']:
-            if hypervisor.lower() == h['name'].lower():
-                self.hypervisor = h['name']
-                return self.hypervisor
-        self.module.fail_json(msg="Hypervisor '%s' not found" % hypervisor)
-
-
-    def get_capabilities(self, key=None):
-        if self.capabilities:
-            return self._get_by_key(key, self.capabilities)
-        capabilities = self.cs.listCapabilities()
-        self.capabilities = capabilities['capability']
-        return self._get_by_key(key, self.capabilities)
-
-
-    def _poll_job(self, job=None, key=None):
-        if 'jobid' in job:
-            while True:
-                res = self.cs.queryAsyncJobResult(jobid=job['jobid'])
-                if res['jobstatus'] != 0 and 'jobresult' in res:
-                    if 'errortext' in res['jobresult']:
-                        self.module.fail_json(msg="Failed: '%s'" % res['jobresult']['errortext'])
-                    if key and key in res['jobresult']:
-                        job = res['jobresult'][key]
-                    break
-                time.sleep(2)
-        return job
-
+# import cloudstack common
+from ansible.module_utils.cloudstack import *
 
 class AnsibleCloudStackTemplate(AnsibleCloudStack):
 
     def __init__(self, module):
         AnsibleCloudStack.__init__(self, module)
-        self.result = {
-            'changed': False,
-        }
 
 
-    def _get_template_args(self, args):
-            args = {}
-            args['displaytext'] = self.module.params.get('displaytext')
-            args['name'] = self.module.params.get('name')
-            args['ostypeid'] = self.get_os_type_id()
-            args['bits'] = self.module.params.get('bits')
-            args['isdynamicallyscalable'] = self.module.params.get('is_dynamically_scalable')
-            args['isextractable'] = self.module.params.get('isextractable')
-            args['isfeatured'] = self.module.params.get('is_featured')
-            args['ispublic'] = self.module.params.get('is_public')
-            args['passwordenabled'] = self.module.params.get('password_enabled')
-            args['requireshvm'] = self.module.params.get('requires_hvm')
-            args['templatetag'] = self.module.params.get('template_tag')
+    def _get_template_args(self):
+        args = {}
+        args['displaytext'] = self.module.params.get('displaytext')
+        if not args['displaytext']:
+          args['displaytext'] = self.module.params.get('name')
+        args['name'] = self.module.params.get('name')
+        args['ostypeid'] = self.get_os_type_id()
+        args['bits'] = self.module.params.get('bits')
+        args['isdynamicallyscalable'] = self.module.params.get('is_dynamically_scalable')
+        args['isextractable'] = self.module.params.get('isextractable')
+        args['isfeatured'] = self.module.params.get('is_featured')
+        args['ispublic'] = self.module.params.get('is_public')
+        args['passwordenabled'] = self.module.params.get('password_enabled')
+        args['requireshvm'] = self.module.params.get('requires_hvm')
+        args['templatetag'] = self.module.params.get('template_tag')
         return args
 
 
@@ -461,18 +245,24 @@ class AnsibleCloudStackTemplate(AnsibleCloudStack):
             for v in vms['virtualmachine']:
                 if vm in [ v['displayname'], v['name'], v['id'] ]:
                     return vm
-        self.module.fail_json(msg="Virtual machine '%s' not found" % vm
+        self.module.fail_json(msg="Virtual machine '%s' not found" % vm )
 
 
     def create_template(self):
         template = self.get_template()
         if not template:
             self.result['changed'] = True
-            args = self._get_template_args(args)
+            args = self._get_template_args()
             vm = self.get_vm()
             args['volumeid'] = vm['rootdeviceid']
             if not self.module.check_mode:
                 template = self.cs.createTemplate(**args)
+                if 'errortext' in template:
+                    self.module.fail_json(msg="Failed: '%s'" % template['errortext'])
+
+                poll_async = self.module.params.get('poll_async')
+                if poll_async:
+                    template = self._poll_job(template, 'template')
         return template
 
 
@@ -480,7 +270,7 @@ class AnsibleCloudStackTemplate(AnsibleCloudStack):
         template = self.get_template()
         if not template:
             self.result['changed'] = True
-            args = self._get_template_args(args)
+            args = self._get_template_args()
             args['url'] = self.module.params.get('url')
             if not args['url']:
                 self.module.fail_json(msg="URL is requried.")
@@ -496,6 +286,13 @@ class AnsibleCloudStackTemplate(AnsibleCloudStack):
             args['sshkeyenabled'] = self.module.params.get('sshkey_enabled')
             if not self.module.check_mode:
                 template = self.cs.registerTemplate(**args)
+
+                if 'errortext' in template:
+                    self.module.fail_json(msg="Failed: '%s'" % template['errortext'])
+
+                poll_async = self.module.params.get('poll_async')
+                if poll_async:
+                    template = self._poll_job(template, 'template')
         return template
 
 
@@ -538,17 +335,17 @@ class AnsibleCloudStackTemplate(AnsibleCloudStack):
     def get_result(self, template):
         if template:
             if 'displaytext' in template:
-                result['displaytext'] = template['displaytext']
+                self.result['displaytext'] = template['displaytext']
             if 'name' in template:
-                result['name'] = template['name']
+                self.result['name'] = template['name']
             if 'zonename' in template:
-                result['zone'] = template['zonename']
+                self.result['zone'] = template['zonename']
             if 'status' in template:
-                result['status'] = template['status']
+                self.result['status'] = template['status']
             if 'created' in template:
-                result['created'] = template['created']
+                self.result['created'] = template['created']
             if 'templatetag' in template:
-                result['template_tag'] = template['templatetag']
+                self.result['template_tag'] = template['templatetag']
         return self.result
 
 
@@ -567,18 +364,19 @@ def main():
             project = dict(default=None),
             zone = dict(default=None),
             template_filter = dict(default='self', choices=[ 'featured', 'self', 'selfexecutable','sharedexecutable','executable', 'community' ]),
-            hypervisor = dict(default=None),
+            hypervisor = dict(required=True, choices=[ 'KVM', 'VMware', 'BareMetal', 'XenServer', 'LXC', 'HyperV', 'UCS', 'OVM' ]),
             requires_hvm = dict(choices=BOOLEANS, default=False),
             password_enabled = dict(choices=BOOLEANS, default=False),
             template_tag = dict(default=None),
             sshkey_enabled = dict(choices=BOOLEANS, default=None),
             is_routing = dict(choices=BOOLEANS, default=False),
-            format = dict(default=None, choices=['QCOW2', 'RAW', 'VHD']),
+            format = dict(default=None, choices=['QCOW2', 'RAW', 'VHD', 'OVA']),
             is_extractable = dict(choices=BOOLEANS, default=False),
             details = dict(default=None),
             bits = dict(default=64, choices=[ 32, 64 ]),
-            displaytext = dict(required=True),
+            displaytext = dict(default=None),
             state = dict(choices=['present', 'absent'], default='present'),
+            poll_async = dict(choices=BOOLEANS, default=True),
             api_key = dict(default=None),
             api_secret = dict(default=None),
             api_url = dict(default=None),
@@ -586,9 +384,6 @@ def main():
         ),
         mutually_exclusive = (
             ['url', 'vm'],
-        ),
-        required_together = (
-            ['vm', 'format'],
         ),
         supports_check_mode=True
     )
